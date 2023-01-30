37b2ddee87e9e1163082b8976c170f2c4f1742e1
vahidoghlu <mr.vahidoghlu@gmail.com>
2023-01-30 19:57:30 +0400

    master update

--- js/carousel.js
+++ js/carousel.js
@@ -1,8 +1,39 @@
 let swiper = new Swiper(".swiper", {
     slidesPerView: 5,
     // centeredSlides: true,
-    spaceBetween: 50,
+    spaceBetween: 20,
     grabCursor: true,
+    autoplay: {
+        delay: 3000,
+    },
+    breakpoints: {
+
+        1200: {
+            freemode:true,
+            slidesPerView: 5,
+            spaceBetween: 20,
+        },
+        900: {
+            freemode:true,
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+        600: {
+            freemode:true,
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+
+        400: {
+            freemode:true,
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        300:{
+            freemode:true,
+            slidesPerView: 1,
+        }
+    }
     // pagination: {
     //     el: ".swiper-pagination",
     //     clickable: true,
@@ -14,4 +45,8 @@ let swiper = new Swiper(".swiper", {
 let swiper1 = new Swiper(".swiper1", {
     effect: "cards",
     grabCursor: true,
+    autoplay: {
+        delay: 2000,
+        pauseOnMouseEnter: true,
+    }
 });
